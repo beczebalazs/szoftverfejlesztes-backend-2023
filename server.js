758ee11e77cdadf140817b8cb3b946b47d2613e5
Lukács Zsombor <lukacs.zsombor@student.ms.sapientia.ro>
2023-11-18 19:53:54 +0200

    db initialization

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
+
+const mongoURI = "mongodb+srv://lukacszsombor:YTFdfoJBFnl62vdW@cluster0.b0vu4xs.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoURI);
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB connection established successfully");
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use((_, res) => {
-  res.send({ message: 'Not found!' });
+app.get("/home", (req, res) => {
+  res.send({ message: 'Welcome to the homepage!' });
 });
 
 app.listen(3001, () => {
