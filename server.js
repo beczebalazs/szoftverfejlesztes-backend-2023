7f4c30d0eecb2d9f78455a92ccad9eb074697a3e
Lukács Zsombor <lukacs.zsombor@student.ms.sapientia.ro>
2023-11-20 18:23:24 +0200

    sign-up-api

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
 
 const mongoURI = "mongodb+srv://lukacszsombor:YTFdfoJBFnl62vdW@cluster0.b0vu4xs.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoURI);
@@ -20,6 +27,29 @@ app.use(cors());
 app.get("/home", (req, res) => {
   res.send({ message: 'Welcome to the homepage!' });
 });
+
+
+app.get("/products", (req, res) => {
+  const filePath = path.join(__dirname, "src/products.json");
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading products.json:", err);
+      res.status(500).send({ error: "Internal Server Error" });
+      return;
+    }
+
+    try {
+      const products = JSON.parse(data);
+      res.json(products);
+    } catch (parseError) {
+      console.error("Error parsing products.json:", parseError);
+      res.status(500).send({ error: "Internal Server Error" });
+    }
+  });
+});
+
+
 
 app.listen(3001, () => {
   console.log("Server is listening on port 3001");
