61a410e33bfadf3c5c3bde0129ddbb65fffa5a7d
fintamas01 <91128770+fintamas01@users.noreply.github.com>
2023-11-28 13:53:17 +0200

    Auth(Database connected)

--- server.js
+++ server.js
@@ -2,145 +2,111 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import jwt from 'jsonwebtoken';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+ 
 const app = express();
-
-const generateAccessToken = userId => {
-	const secretKey = 'our-secret-key';
-	const expiresIn = '12h';
-
-	return jwt.sign({ userId }, secretKey, { expiresIn });
-};
-
+const PORT = 3001;
+ 
 const mongoURI =
 	'mongodb+srv://lukacszsombor:YTFdfoJBFnl62vdW@cluster0.b0vu4xs.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect(mongoURI);
-
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+ 
 const connection = mongoose.connection;
 connection.once('open', () => {
 	console.log('MongoDB connection established successfully');
 });
-
+ 
+const User = mongoose.model(
+	'User',
+	new mongoose.Schema({
+		firstName: String,
+		lastName: String,
+		username: String,
+		email: String,
+		password: String,
+		role: String,
+	}),
+);
+ 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-
+ 
 app.get('/home', (req, res) => {
 	res.send({ message: 'Welcome to the homepage!' });
 });
-
-app.post('/sign-in', (req, res) => {
+ 
+app.post('/sign-in', async (req, res) => {
 	const { email, password } = req.body;
-
-	fs.readFile('src/user.json', 'utf8', (err, data) => {
-		if (err) {
-			console.error(err);
-			res.status(500).send('Error (Reading)');
+ 
+	try {
+		const foundUser = await User.findOne({ email });
+ 
+		if (!foundUser) {
+			res.status(404).send('User not found');
 			return;
 		}
-
+ 
+		if (foundUser.password !== password) {
+			res.status(401).send('Invalid password');
+			return;
+		}
+ 
+		res.status(200).json({ message: 'Login successful', user: foundUser });
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Error querying the database');
+	}
+});
+ 
+app.get('/products', (req, res) => {
+	const filePath = path.join(__dirname, 'src/products.json');
+ 
+	fs.readFile(filePath, 'utf8', (err, data) => {
+		if (err) {
+			console.error('Error reading products.json:', err);
+			res.status(500).send({ error: 'Internal Server Error' });
+			return;
+		}
+ 
 		try {
-			const users = JSON.parse(data);
-			const foundUser = users.find(user => user.email === email);
-
-			if (!foundUser) {
-				res.status(404).send('User not found');
-				return;
+			let products = JSON.parse(data);
+ 
+			const category = req.query.category;
+			if (category) {
+				products = products.filter(product => product.category === category);
 			}
-
-			const encodedPassword = Buffer.from(password).toString('base64');
-			if (foundUser.password !== encodedPassword) {
-				res.status(401).send('Invalid password');
-				return;
-			}
-
-			const access_token = generateAccessToken(foundUser.email);
-			res.status(200).json({ message: 'Login successful', access_token: access_token });
-		} catch (jsonError) {
-			console.error(jsonError);
-			res.status(500).send('Error (Parsing JSON)');
+ 
+			res.json(products);
+		} catch (parseError) {
+			console.error('Error parsing products.json:', parseError);
+			res.status(500).send({ error: 'Internal Server Error' });
 		}
 	});
 });
-
-app.get("/products", (req, res) => {
-  const filePath = path.join(__dirname, "src/products.json");
-
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading products.json:", err);
-      res.status(500).send({ error: "Internal Server Error" });
-      return;
-    }
-
-    try {
-      let products = JSON.parse(data);
-
-      const category = req.query.category;
-      if (category) {
-        products = products.filter(product => product.category === category);
-      }
-
-      res.json(products);
-    } catch (parseError) {
-      console.error("Error parsing products.json:", parseError);
-      res.status(500).send({ error: "Internal Server Error" });
-    }
-  });
-});
-  
-app.post('/sign-up', (req, res) => {
+ 
+app.post('/sign-up', async (req, res) => {
 	const { firstName, lastName, username, email, password, role } = req.body;
-
-	const encodedPassword = Buffer.from(password).toString('base64');
-
+ 
 	const userRole = role || 'member';
-
-	const newUser = {
+ 
+	const newUser = new User({
 		firstName,
 		lastName,
 		username,
 		email,
-		password: encodedPassword,
+		password,
 		role: userRole,
-		access_token: generateAccessToken(email),
-	};
-
-	fs.readFile('src/user.json', 'utf8', (err, data) => {
-		if (err) {
-			console.error(err);
-			res.status(500).send('Error (Reading)');
-			return;
-		}
-
-		try {
-			const existingUsers = JSON.parse(data) || [];
-
-			existingUsers.push(newUser);
-
-			fs.writeFile('src/user.json', JSON.stringify(existingUsers), writeErr => {
-				if (writeErr) {
-					console.error(writeErr);
-					res.status(500).send('Error (Saving)');
-				} else {
-					res.status(200).json({ message: 'User registered', access_token: newUser.access_token });
-				}
-			});
-		} catch (jsonError) {
-			console.error(jsonError);
-			res.status(500).send('Error (Parsing JSON)');
-		}
 	});
+ 
+	try {
+		const savedUser = await newUser.save();
+		res.status(200).json({ message: 'User registered', user: savedUser });
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Error saving to the database');
+	}
 });
-
-app.listen(3001, () => {
-	console.log('Server is listening on port 3001');
+ 
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
 });
-
