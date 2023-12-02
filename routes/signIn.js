525e238c53c76869d6d7275faf8b3972be434c67
Lukács Zsombor <lukacs.zsombor@student.ms.sapientia.ro>
2023-12-02 19:07:10 +0200

    delete-successful-mesage(sign-in)

--- routes/signIn.js
+++ routes/signIn.js
@@ -5,29 +5,29 @@ import User from '../models/User.js';
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  try {
-    const foundUser = await User.findOne({ email });
+	try {
+		const foundUser = await User.findOne({ email });
 
-    if (!foundUser) {
-      res.status(404).send('User not found');
-      return;
-    }
+		if (!foundUser) {
+			res.status(404).send('User not found');
+			return;
+		}
 
-    // Ellenőrizze a jelszó helyességét bcrypt segítségével
-    const passwordMatch = await bcrypt.compare(password, foundUser.password);
+		const passwordMatch = await bcrypt.compare(password, foundUser.password);
 
-    if (!passwordMatch) {
-      res.status(401).send('Invalid password');
-      return;
-    }
+		if (!passwordMatch) {
+			res.status(401).send('Invalid password');
+			return;
+		}
 
-    res.status(200).json({ message: 'Login successful', user_id: foundUser._id });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error querying the database');
-  }
+		res.status(200).json({ user_id: foundUser._id });
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Error querying the database');
+	}
 });
 
 export default router;
+
