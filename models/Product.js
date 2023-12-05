99bf5c9c762d4f03086ea8bdabe42bd64f5e8a11
fintamas01 <91128770+fintamas01@users.noreply.github.com>
2023-12-05 23:34:58 +0200

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,18 +1,27 @@
 import mongoose from 'mongoose';
+import Review from './Review.js';
 
-const Product = mongoose.model(
-	'Product',
-	new mongoose.Schema({
-		images: [String],
-		title: String,
-		price: Number,
-		rating: Number,
-		category: String,
-		brand: String,
-		shortDescription: String,
-		longDescription: String,
-	}),
-);
+const ProductSchema = new mongoose.Schema({
+	images: [String],
+	title: String,
+	price: Number,
+	rating: Number,
+	category: String,
+	brand: String,
+	shortDescription: String,
+	longDescription: String,
+	averageRating: Number,
+});
+
+
+ProductSchema.pre('save', async function (next) {
+	const reviews = await Review.find({ productId: this._id });
+	const totalRating = reviews.reduce((sum, review) => sum + review.rating, 0);
+	this.averageRating = reviews.length > 0 ? totalRating / reviews.length : 0;
+	next();
+});
+
+const Product = mongoose.model('Product', ProductSchema);
 
 export default Product;
 
