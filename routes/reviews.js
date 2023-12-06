a6c956976644a6ddacbbb13cb3c9fe051f1be384
fintamas01 <91128770+fintamas01@users.noreply.github.com>
2023-12-06 11:54:33 +0200

    Update reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,3 +1,11 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Review from '../models/Review.js';
+
+const router = express.Router();
+
+router.use(express.json());
+
 router.post('/', async (req, res) => {
   const { productId, username, description, rating, date } = req.body;
 
@@ -23,3 +31,18 @@ router.post('/', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+router.get('/:productId', async (req, res) => {
+  const productId = req.params.productId;
+
+  try {
+    const reviews = await Review.find({ productId });
+
+    res.status(200).json(reviews);
+  } catch (error) {
+    console.error('Error querying the database:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+export default router;
